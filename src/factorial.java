fd5608f1f48396eb3d3daa261c4e4ab2f62cf03b
J0sH-code <joshrylesanteno07@gmail.com>
2026-07-30 22:24:26 +0800

    Add recursive method to compute factorial of a number

--- src/factorial.java
+++ src/factorial.java
@@ -1,6 +1,7 @@
 public class factorial {
     public static void main(String[] args) {
         System.out.println(getFactorial(5));
+        System.out.println(recursiveFactorial(5));
     }
 
     static int getFactorial(int num){
@@ -8,6 +9,17 @@ public class factorial {
         for (int i = 0; i <= 4; i++) {
             finalAnswer = finalAnswer * (num-i);
         }
+        return finalAnswer;
+    }
+
+    static int recursiveFactorial(int num){
+        int finalAnswer = 1;
+        if (num == 0) {
+            return 1;
+        } else {
+            finalAnswer = finalAnswer * (num * recursiveFactorial(num-1)); 
+        }
+
         return finalAnswer;
     }
 }
