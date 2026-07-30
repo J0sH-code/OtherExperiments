f5ea9bf0c0e5c669df2fee9fd6d54c856d812f45
J0sH-code <joshrylesanteno07@gmail.com>
2026-07-30 22:16:33 +0800

    Add factorial class with method to compute factorial of a number

--- src/factorial.java
+++ src/factorial.java
@@ -1,10 +1,9 @@
-
-public class trial {
+public class factorial {
     public static void main(String[] args) {
-        System.out.println(factorial(5));
+        System.out.println(getFactorial(5));
     }
 
-    static int factorial(int num){
+    static int getFactorial(int num){
         int finalAnswer = 1;
         for (int i = 0; i <= 4; i++) {
             finalAnswer = finalAnswer * (num-i);
